4fda8a792f52c10664652717ceb6800cc8cb9903
WhereIsHeroFrom <menjitianya2007@163.com>
2024-05-10 19:54:01 +0800

    第41课 部署服务器 第42课 公网IP 和 端口

--- server/main.py
+++ server/main.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 current_path = os.path.abspath(__file__)
-top_path = "\\".join(current_path.split('\\')[:-2])
+top_path = "/".join(current_path.split('/')[:-2])
 sys.path.append(top_path)
 from share.const import *
 from game import *
@@ -25,7 +25,7 @@ async def handle_client(reader, writer):
     await writer.drain()
 
 async def main():
-    server = await asyncio.start_server(handle_client, '127.0.0.1', const.LISTEN_PORT)
+    server = await asyncio.start_server(handle_client, '0.0.0.0', const.LISTEN_PORT)
     print('Server start! Listen on port:', const.LISTEN_PORT)
 
     async with server:
