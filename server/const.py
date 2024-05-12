7f86c71689cef45fd824ce8b5f34f510f191178f
WhereIsHeroFrom <menjitianya2007@163.com>
2024-05-12 08:10:38 +0800

    第45-48课 数据库相关

--- server/const.py
+++ server/const.py
@@ -1,2 +1,7 @@
 LISTEN_PORT = 8888
 MAX_BYTES = 10240
+
+DB_HOST = 'zty19880502.mysql.polardb.rds.aliyuncs.com'
+DB_PORT = 3306
+DB_USER = 'zty'
+DB_PASS = '08416263aAa'
--- server/game.py
+++ server/game.py
@@ -1,16 +1,47 @@
-import sys
-import os
-
-current_path = os.path.abspath(__file__)
-top_path = "\\".join(current_path.split('\\')[:-2])
-sys.path.append(top_path)
-
 from share.const import * 
+from const import *
+import pymysql
+import json
 
 class Game(object):
     def __init__(self):
-        self.hasPlant = [ [0]*GRID_COUNT[1] for _ in range(GRID_COUNT[0]) ]
+        self.plantInfo = [ [0]*GRID_COUNT[1] for _ in range(GRID_COUNT[0]) ]
+        self.connectMysql()
+        self.loadPlantInfo()
 
+    def connectMysql(self):
+        config = {
+            'host': DB_HOST,
+            'port': DB_PORT,
+            'user': DB_USER,
+            'password': DB_PASS,
+            'db': 'pvzdb',
+            'charset': 'utf8mb4'
+        }
+        self.connection = pymysql.connect(**config)
+
+    def executeSql(self, sqlcmd):
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(sqlcmd)
+            self.connection.commit()
+            results = cursor.fetchall()
+            return results
+        except Exception as e:
+            print("execute failed!", sqlcmd, e)
+            return []
+    
+    def loadPlantInfo(self):
+        results = self.executeSql("select * from game;")
+        if len(results) == 0:
+            self.savePlantInfo()
+        else:
+            self.plantInfo = json.loads( results[0] )
+
+    def savePlantInfo(self):
+        plantInfo = json.dumps(self.plantInfo)
+        self.executeSql( "INSERT INTO game (id, plantInfo) VALUES (0, '%s') ON DUPLICATE KEY UPDATE plantInfo = '%s';" % (plantInfo, plantInfo) )
+        
     def checkAddPlant(self, pos, plant_idx):
         msg = {
             'type' : S2C_ADD_PLANT,
@@ -24,10 +55,11 @@ class Game(object):
             return msg
         if y < 0 or y >= GRID_COUNT[1]:
             return msg
-        if self.hasPlant[x][y] != 0:
+        if self.plantInfo[x][y] != 0:
             return msg
         # 金钱判定
 
-        self.hasPlant[x][y] = plant_idx
+        self.plantInfo[x][y] = plant_idx
+        self.savePlantInfo()
         msg['code'] = S2C_CODE_SUCCEED
         return msg
